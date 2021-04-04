3e80757f94841c84a82360ddaa23fccb2710d881
Igor <e.igor@hotmail.com>
2021-04-04 13:48:04 -0300

    Atualização do Código

--- assinaturas.php
+++ assinaturas.php
@@ -5,31 +5,7 @@
 	<title>Assinatura Terumo</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="shortcut icon" href="favicon.ico" />
-<style>
-p{
-	color:#008D61;
-	font-family:'Century Gothic', CenturyGothic, AppleGothic, sans-serif;
-	font-size: 18px;
-}
-h4, h5{
-	font-family:'Century Gothic', CenturyGothic, AppleGothic, sans-serif;
-	font-size: 14px;
-}
-table, th, td {
-    border: 0px solid black;
-    border-collapse: collapse;
-	margin-left: 20px; 
-}
-table {
-	margin: 0 auto;
-	width: 50%;
-	top: 50%;
-}
-form {
-	margin: 0 auto;
-	width: 98%;
-}
-</style>
+	<link rel="stylesheet" href="_css/estilo.css"/>
 </head>
 <body>
 	<br/>
@@ -45,13 +21,13 @@ form {
 /* DADOS ASSINATURA */
 			echo "<p><b>$nome</b><sub><br/><i>$cargo</i></sub></p>";
 			echo "<h5>
-			<b>Terumo Medical do Brasil Ltda</b>
-			<br/>P&ccedil;a General Gentil Falc&atildeo,108 - 9&deg; Andar
-			<br/>Cidade Mon&ccedil;&otildees - S&atildeo Paulo-SP CEP:04571-150
+			<b>Empresa Ltda</b>
+			<br/>Endereço da empresa, nº
+			<br/>Bairro - Cidade - Estado CEP: 0000-000
 			<br/>Tel: $telefone | Celular: $celular<br/>Email: $email</h4>";
 		?>
 	</td>
-		<td style="border-left:solid #008D61;"><img src="TERUMO_LOGO.jpg" width="400">
+		<td style="border-left:solid #008D61;"><img src="_imagens/logo_google.jpg" width="300">
 		</td>
 	</tr>
 	</tbody>
